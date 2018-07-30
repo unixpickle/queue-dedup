f0a5873c2c7f5c574865d8fedb39c0acfb9db585
Alex Nichol <unixpickle@gmail.com>
2018-07-30 13:01:46 -0400

    fix plugin after page refresh

--- background.js
+++ background.js
@@ -1,15 +1,4 @@
 chrome.runtime.onInstalled.addListener(() => {
-    chrome.pageAction.onClicked.addListener((tab) => {
-        const query = {
-            active: true,
-            currentWindow: true
-        };
-        chrome.tabs.query(query, (tabs) => {
-            const mainTab = tabs[0];
-            chrome.tabs.executeScript(mainTab.id, { file: 'inject.js' });
-        });
-    });
-
     chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
         chrome.declarativeContent.onPageChanged.addRules([{
             conditions: [
@@ -22,5 +11,16 @@ chrome.runtime.onInstalled.addListener(() => {
             ],
             actions: [new chrome.declarativeContent.ShowPageAction()]
         }]);
+    });
+});
+
+chrome.pageAction.onClicked.addListener((tab) => {
+    const query = {
+        active: true,
+        currentWindow: true
+    };
+    chrome.tabs.query(query, (tabs) => {
+        const mainTab = tabs[0];
+        chrome.tabs.executeScript(mainTab.id, { file: 'inject.js' });
     });
 });
